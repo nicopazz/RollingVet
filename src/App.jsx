54005aee4cf3e20064919e6f231e4e8c651048f6
Nicolas Paz <nicopazmalizia@gmail.com>
2025-12-03 00:46:38 -0300

    Inicializo frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola RollingVet!</h1>
+      <p>Frontend inicializado correctamente.</p>
+    </div>
+  );
+}
+
+export default App;
